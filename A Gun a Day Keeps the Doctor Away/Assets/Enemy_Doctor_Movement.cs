c59859aeed82f7c7dc833eb965898014f2e19569
Brandon Nguyen <brandvn1@uci.edu>
2019-04-13 00:00:26 -0700

    t status

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Enemy_Doctor_Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Doctor_Movement : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    private Transform target;
+    public float DashState;
+    public bool canDocDash = true;
+    public Vector2 savedVelocity;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DashState > 0)
+        {
+            canDocDash = false;
+            DashState -= Time.deltaTime;
+        }
+
+        
+        if (DashState <= 0)
+        {
+            canDocDash = true;
+        }
+
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+           
+        }
+
+    }
+}
