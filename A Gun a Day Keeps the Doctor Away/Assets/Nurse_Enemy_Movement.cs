804ee8c91978ad1b6a6d5f22217c9f96cbeb6a2f
Misplacedtext <43836149+Misplacedtext@users.noreply.github.com>
2019-04-12 21:43:51 -0700

    Enemy Movement for Nurse

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Nurse_Enemy_Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nurse_Enemy_Movement : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    private Transform target;
+
+    void Awake()
+    {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+
+}
