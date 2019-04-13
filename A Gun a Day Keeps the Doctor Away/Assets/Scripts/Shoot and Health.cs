5248ccdea58e9aa448c291a2635a52459271a2f7
Annie Zhao <anniezhao107@gmail.com>
2019-04-12 21:30:54 -0700

    shootandhealth lots of if statements

--- A Gun a Day Keeps the Doctor Away/Assets/Scripts/Shoot and Health.cs
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/Shoot and Health.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootandHealth : MonoBehaviour
 {
+    public int health = 100;
+    public string currentWeap = 0;
+    public int range = 10;
+    public int weapDamage = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,58 @@ public class ShootandHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (health >= 125)
+        {
+            GameOver();
+        }
+        else if (health >= 67)
+        {
+            currentWeap = 0;
+        }
+        else if (health >= 34)
+        {
+            currentWeap = 1;
+        }
+        else
+        {
+            currentWeap = 2;
+        }
         
+        if (Input.GetMouseButtonDown(1))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if (currentWeap == 0)
+        {
+            range = 10;
+            weapDamage = 20;
+        }
+        else if (currentWeap == 1)
+        {
+            range = 30;
+            weapDamage = 20;
+        }
+        else
+        {
+            range = 50;
+            weapDamage = 20;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.gameObject.tag == "enemy")
+        {
+            health += 10;
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over");
     }
 }
