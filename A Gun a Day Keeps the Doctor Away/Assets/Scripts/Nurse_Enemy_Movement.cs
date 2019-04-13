38c75e7438b46ec3873be6c47dce266aeab42b19
Misplacedtext <43836149+Misplacedtext@users.noreply.github.com>
2019-04-13 11:14:06 -0700

    Additional Nurse Score? + Death? + Animation code?

--- A Gun a Day Keeps the Doctor Away/Assets/Scripts/Nurse_Enemy_Movement.cs
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/Nurse_Enemy_Movement.cs
@@ -8,23 +8,69 @@ public class Nurse_Enemy_Movement : MonoBehaviour
     public float stoppingDistance;
     private Transform target;
 
+    public static int score;
+    public string text;
+
+
+    public int NurseMaxHealth = 50;
+    public int NurseCurHealth;
+    public int scoreValue = 10;
+    bool isDead;
+    public AudioClip nurseDeath;
+
+    Animator anim;
+    AudioSource NurseAudio;
+
     void Awake()
     {
         target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        NurseAudio = GetComponent<AudioSource>();
+        NurseCurHealth = NurseMaxHealth;
+
+        score = 0;
     }
     // Start is called before the first frame update
     void Start()
     {
 
     }
+    void NurseDead()
+    {
+        isDead = true;
+        anim.SetTrigger("Dead");
+        NurseAudio.clip = nurseDeath;
+        NurseAudio.Play();
+    }
+    public void TakeDamage (int amount, Vector3 hitpoint)
+    {
+        if (isDead)
+        {
+            return;
+        }
+        NurseAudio.Play();
+        NurseCurHealth -= amount;
+        if(NurseCurHealth <= 0)
+        {
+            NurseDead();
+        }
+    }
+
+    public void deSpawn()
+    {
+        //despwan the nurse
+        Destroy(gameObject, 3f);
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //chase coding
         if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
+        //Still need to add the aditional score portion
     }
 
 
