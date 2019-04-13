2f4cbb06ef0d450153ac3c774ee87b1874a8e0f5
Annie Zhao <anniezhao107@gmail.com>
2019-04-13 15:16:46 -0700

    main menu and instructions

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
