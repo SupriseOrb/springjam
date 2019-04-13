b1d6db1441cf0a0e0f0c7c0e257632d13808a9a5
Brandon Nguyen <brandvn1@uci.edu>
2019-04-12 23:25:15 -0700

    Potential Poison Mechanic

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Poison.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poison : MonoBehaviour
+{
+    public int MaxHealth = 125;
+    public int curHealth = 100;
+    public float healthBarLength;
+
+    public bool isPoisoned = false;
+    bool poisonEffect = true;
+    public int poisonDamage = 5;
+
+    public float poisonTimer;
+
+    private void Awake()
+    {
+        healthBarLength = Screen.width / 3;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    IEnumerator PoisonDamage ()
+    {
+        if (poisonEffect)
+        {
+            curHealth -= poisonDamage;
+            poisonEffect = false;
+            yield return new WaitForSeconds(poisonTimer);
+            poisonEffect = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    { 
+        if (isPoisoned== true);
+        {
+            StartCoroutine("PoisonDamage");
+        }
+    }
+   
+}
