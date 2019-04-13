96222dee78566512754ea49de565decc2dae6ede
Annie Zhao <anniezhao107@gmail.com>
2019-04-12 22:00:32 -0700

    player bullet script

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float velY = 5f;
+    public int travelled = 0;
+    public int range;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        range = 30;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(0,velY);
+        travelled += velY;
+        if (travelled == 0)
+        {
+            //
+        }
+    }
+}
