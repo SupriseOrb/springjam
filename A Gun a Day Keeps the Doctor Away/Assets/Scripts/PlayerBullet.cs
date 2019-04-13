25fc784f548fac41bce62f760c0e9f05a6143e9f
Jesse <jesseallas@gmail.com>
2019-04-13 11:39:47 -0700

    pewpew bulllets

--- A Gun a Day Keeps the Doctor Away/Assets/Scripts/PlayerBullet.cs
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/PlayerBullet.cs
@@ -9,19 +9,27 @@ public class PlayerBullet : MonoBehaviour
     public int travelled = 0;
     public int range;
     public GameObject theplayer;
-    Rigidbody2D rb;
+    //Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+       // rb = GetComponent<Rigidbody2D>();
         //range = theplayer.range;
     }
 
+    private void Awake()
+    {
+        //transform.rotation.z = theplayer.transform.rotation.z;
+        Vector3 newRotation = new Vector3(theplayer.transform.eulerAngles.x, theplayer.transform.eulerAngles.x, theplayer.transform.eulerAngles.z);
+        gameObject.transform.eulerAngles = newRotation;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(velX,velY);
+        transform.Translate(0, 5 * Time.deltaTime, 0);
+
+        //rb.velocity = new Vector2(velX,velY);
         //travelled += velY;
         if (travelled >= range)
         {
