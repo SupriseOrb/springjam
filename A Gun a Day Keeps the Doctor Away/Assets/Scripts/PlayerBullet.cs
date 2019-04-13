5b8ad583fe43b3219dbbd06a96eb2e34ae6e4842
Jesse <jesseallas@gmail.com>
2019-04-13 11:47:02 -0700

    bullet works foreal now

--- A Gun a Day Keeps the Doctor Away/Assets/Scripts/PlayerBullet.cs
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/PlayerBullet.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
-    public float velY = 5f;
-    public float velX = 0;
-    public int travelled = 0;
-    public int range;
+    public float speed;
+     float travelled = 0;
+    public float range;
     public GameObject theplayer;
-    //Rigidbody2D rb;
-    // Start is called before the first frame update
+
     void Start()
     {
-       // rb = GetComponent<Rigidbody2D>();
-        //range = theplayer.range;
+  
     }
 
     private void Awake()
     {
-        //transform.rotation.z = theplayer.transform.rotation.z;
         Vector3 newRotation = new Vector3(theplayer.transform.eulerAngles.x, theplayer.transform.eulerAngles.x, theplayer.transform.eulerAngles.z);
         gameObject.transform.eulerAngles = newRotation;
     }
@@ -27,12 +23,11 @@ public class PlayerBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 5 * Time.deltaTime, 0);
-
-        //rb.velocity = new Vector2(velX,velY);
-        //travelled += velY;
+        transform.Translate(0, speed * Time.deltaTime, 0);
+        travelled += Time.deltaTime;
         if (travelled >= range)
         {
+            Destroy(gameObject);
             //destroy
         }
     }
