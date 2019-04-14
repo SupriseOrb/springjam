10e084a34a892d30fcd24c4128a7c02d39db89ce
Jesse <jesseallas@gmail.com>
2019-04-13 18:40:09 -0700

    Click Click Boom

--- A Gun a Day Keeps the Doctor Away/Assets/Scripts/ShootandHealth.cs
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/ShootandHealth.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class ShootandHealth : MonoBehaviour
 {
     static int statichealth = 85;
-    public int health = 85;
+    public int health = 67;
     public int currentWeap = 0;
     public int range = 10;
     public int tick = 0; //tracks how many frames has passed to know when to decrease health again
@@ -15,11 +15,18 @@ public class ShootandHealth : MonoBehaviour
     public float pistolrate;
     public float gatlingrate;
     public float reloadtime;
-
+    public AudioSource shotgunload;
+    public AudioSource shotgunshoot;
+    public AudioSource pistolload;
+    public AudioSource pistolshoot;
+    public AudioSource gatlingload;
+    public AudioSource gatlingshoot;
     public Sprite lowhp;
     public Sprite medhp;
     public Sprite hihp;
     SpriteRenderer sr;
+    float nextSoundTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,16 +48,69 @@ public class ShootandHealth : MonoBehaviour
         }
         else if (health <= 100 && health >= 66)
         {
+            if (health == 100)
+            {
+                
+                if (!pistolload.isPlaying)
+                {
+                    pistolload.Play();
+                }
+            }
+            if (health == 66)
+            {
+                if (!pistolload.isPlaying)
+                {
+                    pistolload.Play();
+                }
+            }
+
             currentWeap = 0;
             sr.sprite = hihp;
         }
         else if (health <= 65 && health >= 33)
         {
+            if (health == 65)
+            {
+                if (!shotgunload.isPlaying)
+                {
+                    shotgunload.Play();
+                }
+            }
+            if (health == 33)
+            {
+                if (shotgunload.isPlaying)
+                {
+                    shotgunload.Play();
+                }
+            }
             currentWeap = 1;
             sr.sprite = medhp;
         }
         else if (health <= 32 && health >= 0)
         {
+            if (health == 32)
+            {
+                gatlingload.Play();
+                health -= 1;
+            }
+            if (health == 0)
+            {
+                if (health == 32)
+                {
+                    if (gatlingload.isPlaying)
+                    {
+                        gatlingload.Play();
+                    }
+                }
+                if (health == 0)
+                {
+                    if (gatlingload.isPlaying)
+                    {
+                        gatlingload.Play();
+                    }
+                }
+            }
+
             currentWeap = 2;
             sr.sprite = lowhp;
         }
@@ -74,8 +134,8 @@ public class ShootandHealth : MonoBehaviour
         //pistol
         if (currentWeap == 0 && reloadtime >= pistolrate)
         {
-           
-                Instantiate(bullet, transform.position, transform.rotation);
+            pistolshoot.Play();
+            Instantiate(bullet, transform.position, transform.rotation);
                 reloadtime = 0;
             
 //            range = 10;
@@ -83,7 +143,7 @@ public class ShootandHealth : MonoBehaviour
         //shotgun
         else if (currentWeap == 1 && reloadtime >= shotgunrate)
         {
-
+            shotgunshoot.Play();
             GameObject bullet1 = Instantiate(bullet, transform.position, transform.rotation);
             GameObject bullet2 = Instantiate(bullet, transform.position, transform.rotation);
             bullet2.transform.Rotate(Vector3.forward * 10);
@@ -106,6 +166,13 @@ public class ShootandHealth : MonoBehaviour
         //gatling
         else if (currentWeap == 2 && reloadtime >= gatlingrate)
         {
+             // can't play sounds if before this time
+                                     // can't play if last one not finished:
+            if (Time.time >= nextSoundTime)
+            {
+                gatlingshoot.Play();
+                nextSoundTime = Time.time + 0.1f;
+            }
                 GameObject bullet6 = Instantiate(bullet, transform.position, transform.rotation);
                 GameObject bullet7 = Instantiate(bullet, transform.position, transform.rotation);
             GameObject bullet8 = Instantiate(bullet, transform.position, transform.rotation);
