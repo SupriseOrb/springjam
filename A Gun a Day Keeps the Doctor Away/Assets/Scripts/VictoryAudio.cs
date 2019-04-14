8a58d44a8d96ef97b0cc44e5208600883c4686ad
Annie Zhao <anniezhao107@gmail.com>
2019-04-14 16:33:51 -0700

    different audio

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/VictoryAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource theaudio;
+    void Start()
+    {
+        theaudio = this.GetComponent<AudioSource>();
+        if (Scoring.ranOutOfHealth == true)
+        {
+            theaudio.gameObject.SetActive(false);
+        }
+    }
+    
+}
