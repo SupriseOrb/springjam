20c83d354192d0f875a8b9a36b1378dcaa5f7f97
Misplacedtext <43836149+Misplacedtext@users.noreply.github.com>
2019-04-14 13:05:00 -0700

    variable spawner code

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Variable_Spawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable_Spawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+    int enemyNo;
+    public float maxPos = 2.5f;
+    public float spawnInterval = 3f;
+    public float currentSpawnTime = 0;
+
+    public float midCountdown = 15;
+    public float currentCountdownTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void spawn()
+    {
+        Vector3 enemyPos = new Vector3(Random.Range(-2.8f, 2.8f), transform.position.y, transform.position.z);
+        enemyNo = Random.Range (0,27);
+        Instantiate(enemies[enemyNo], enemyPos, transform.rotation);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        currentSpawnTime += Time.deltaTime;
+        currentCountdownTime += Time.deltaTime;
+        
+        if (currentSpawnTime >= spawnInterval)
+        {
+            spawn();
+            currentSpawnTime = 0;
+        }
+
+        if (currentCountdownTime >= midCountdown)
+        {
+            spawnInterval -= 0.1f;
+            currentCountdownTime = 0;
+        }
+
+    }
+
+}
