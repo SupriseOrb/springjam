3b224c1f2b33dfc10b8adf92f00fd40085cf5ac7
Misplacedtext <43836149+Misplacedtext@users.noreply.github.com>
2019-04-14 09:49:42 -0700

    added potential cheat code script

---
+++ A Gun a Day Keeps the Doctor Away/Assets/Cheat_Code_Script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheat_Code_Script : MonoBehaviour
+{
+    private string[] cheatCode;
+    private int index;
+
+    private void Awake()
+    {
+        //Cheat Code is catzrkewl
+        cheatCode = new string[] { "c", "a", "t", "z", "r", "k", "e", "w", "l" };
+        index = 0;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(cheatCode[index]))
+            {
+                //+1 to index to check the next key in the code 
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+        if (index == cheatCode.Length)
+        {
+            //cheatcode successfully implemented
+            //Stick cat stuff here
+
+        }
+    }
+}
