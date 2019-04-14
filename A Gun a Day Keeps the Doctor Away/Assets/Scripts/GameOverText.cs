06e6392ba530bd85a56c839c201e03828b636652
Annie Zhao <anniezhao107@gmail.com>
2019-04-13 21:20:47 -0700

    trying to do scoring system across scenes

--- A Gun a Day Keeps the Doctor Away/Assets/Scripts/GameOverText.cs
+++ A Gun a Day Keeps the Doctor Away/Assets/Scripts/GameOverText.cs
@@ -12,13 +12,22 @@ public class GameOverText : MonoBehaviour
     void Start()
     {
         overtext = gameObject.GetComponent<Text>();
-        scoreobj = GameObject.Find("Score");
-        scoreScript = scoreobj.GetComponent<Scoring>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        overtext.text = "Daddy Riley lives! Here's how much you helped him make: \n" + scoreScript.score.ToString();
+        scoreobj = GameObject.Find("Score");
+        scoreScript = scoreobj.GetComponent<Scoring>();
+        Debug.Log(scoreScript);
+        if (scoreScript.ranOutOfHealth == true)
+        {
+            overtext.text = "Daddy Riley lives! Here's how much you helped him make: \n" + scoreScript.score.ToString();
+        }
+        if (scoreScript.ranOutOfHealth == false)
+        {
+            overtext.text = "You didn't keep Daddy Riley alive! You can't cash out your: \n" + scoreScript.score.ToString();
+        }
     }
 }
